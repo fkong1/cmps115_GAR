ea67d03bd14ed1818d9105e4ef58262b04df9e40
jinjunzhen <jzhen45@ucsc.edu>
2018-11-10 10:05:07 -0800

    Merge branch 'master' of /Users/jinjunzhen/PycharmProjects/cmps115_GAR with conflicts.

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import mysql.connector
 
-from bottle import get, route, run, template, view, static_file, request, post
-__author__ = 'kai'
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="Alliance"
+)
 
-from flask import Flask
+print("---database connect: ---" + str(mydb))
 
-app = Flask(__name__)
+from bottle import get, route, run, template, view, static_file
 
 @get('/')
 def index():
@@ -15,141 +18,21 @@ def index():
 def register():
     return template("register_temp")
 
-@get('/find-password')
-def register():
+@get('/register-succeed')
+def register_succeed():
+    return template("register_succeed")
+
+@get('/find-password-succeed')
+def password_succeed():
+    return template("password-succeed")
+
+@get('/findpw')
+def find_pw():
     return template("findPW_temp")
 
 @get('/main')
 def main():
     return template("main_temp")
-
-
-def login_connectDB(username, password):
-    mydb = mysql.connector.connect(
-    host = "localhost",
-    user="root",
-    passwd="password",
-    database="gar_database"
-)
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT username FROM user")
-    myresult = mycursor.fetchone()
-    print ("myresult: " + str(myresult))
-    if myresult is not None:
-        for x in myresult:
-            usern = x
-        if usern == username:
-            print "password is correct"
-            return True
-        else:
-            print "username is wrong"
-            return False
-    mydb.close()
-
-def register_connectDB(username, password,cruzid,studentid,emailaddress,identity):
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user="root",
-        passwd="password",
-        database="gar_database"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT username FROM user")
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        if x[0] == username:
-            return False
-    mycursor.execute("SELECT emailaddress FROM user")
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        if x[0] == emailaddress:
-            return False
-    sql = "INSERT INTO user (username, password,cruzid,studentid,emailaddress,identity) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (username, password,cruzid,studentid,emailaddress,identity)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    return True
-    mydb.close()
-
-def findPW_connectDB(choice, value):
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user="root",
-        passwd="password",
-        database="gar_database"
-    )
-    mycursor = mydb.cursor()
-    if choice == "username":
-        mycursor.execute("SELECT username FROM user")
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            if x[0] == value:
-                return True
-    else:
-        mycursor.execute("SELECT emailaddress FROM user")
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            if x[0] == value:
-                return True
-    return False
-    mydb.close()
-def findEmail(username):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="password",
-        database="gar_database"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT username, emailaddress FROM user")
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        if x[0] == username:
-            return x[1]
-
-@post('/login')
-def login():
-    login_username = request.forms.get('username')
-    login_password = request.forms.get('password')
-
-    print("login_username: " + str(login_username))
-    print("login_password: " + str(login_password))
-    if login_connectDB(login_username,login_password)== False:
-        return template("login_wrong")
-    else:
-        return template("login_succeed")
-
-@post('/register')
-def register():
-    register_username = request.forms.get('username')
-    register_password = request.forms.get('password')
-    register_cruzid = request.forms.get('cruzid')
-    register_studentid = request.forms.get('studentid')
-    register_emailaddress = request.forms.get('emailaddress')
-    register_identity = request.forms.get('identity')
-    if register_password == "" or register_username == "" or register_cruzid == "" or register_studentid == "" or register_emailaddress == "":
-        return template("register_wrong")
-    if register_connectDB(register_username,register_password,register_cruzid,register_studentid,register_emailaddress,register_identity) == True:
-        return template("register_succeed")
-    else:
-        return template("registe_used")
-
-
-@post('/find-password')
-def password_succeed():
-    find_choice = request.forms.get('choice')
-    register_username = request.forms.get('username')
-    register_email = request.forms.get('emailaddress')
-    if find_choice == "username":
-        if findPW_connectDB(find_choice, register_username) == True:
-            email = findEmail(register_username)
-            print email
-            return template("findPW_succeed")
-    else:
-        if findPW_connectDB(find_choice, register_email) == True:
-            return template("findPW_succeed")
-    return template("findPW_wrong")
-
 
 
 # Let's add some code to serve jpg images from our static images directory.
@@ -163,5 +46,4 @@ def serve_css(filename):
     return static_file(filename, root='css', mimetype='text/css')
 
 
-run(reloader=True, host='localhost', port=3005)
-
+run(reloader=True, host='localhost', port=8080)
